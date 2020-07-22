c84216ef8af0257b8b0bb4b3510219afa39bb414
Revital Tarnovsky <revit91@gmail.com>
2020-07-22 10:03:43 +0300

    22 7 20

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const auth = async(req,res,next) => {
+  const token = req.header("x-auth-token");
+  // אומר שלא נשלח הידר בכלל ואז נציג שגיאה
+  if(!token) {
+    return res.status(400).json({ message: "error token you must send header" })
+  }
+  try{
+    const decode = jwt.verify(token,config.get("tokenKey"));
+    req.decoded = decode;
+    next()
+  }
+  catch{
+    // אם מגיע לקצ' אומר שהטוקן לא תקין ולא תואם לסיקריט שלנו
+    return res.status(400).json({ message: "error token 2 not valid" })
+  }
+}
+module.exports = auth;
---
+++ mongoDb/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://koko:MONKEYS12@cluster0-2b8gk.gcp.mongodb.net/shop_market?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+ console.log("connected")
+});
+
+module.exports = db;
